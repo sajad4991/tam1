6fe6293611ea6cc2376c6b29438f44797f101ff5
sajad4991 <124254656+sajad4991@users.noreply.github.com>
2023-02-21 14:43:42 +0330

    Update .py

--- .py
+++ .py
@@ -1,8 +1,8 @@
 a = input("har chi dos dari az keybordet vard kon : ")
 b = len(a)
-for i in range(b+1):
-    if a[i] == '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' :
-        print("string have numbers")
+for i in range(b):
+     if a[i] == '0' or a[i] == '1' or a[i] == '2' or a[i] == '3' or a[i] == '4' or a[i] == '5' or a[i] == '6' or a[i] == '7' or a[i] == '8' or a[i] == '9':
+        print("OH !! *string has NUMBER*")
         break        
-    else :
-         print("a")
+else :
+    print(a)
